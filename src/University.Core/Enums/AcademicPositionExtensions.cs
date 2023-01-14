ca941aaa3b17788b31860748b085f6cda81d1f63
Serhii Khudolii <s.khudolii@gmail.com>
2023-01-15 01:29:48 +0200

    Enum Refactoring

---
+++ src/University.Core/Enums/AcademicPositionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Core.Enums
+{
+    public class AcademicPositionExtensions
+    {
+        public static string GetAcademicPositionAbbreviation(AcademicPosition academicPosition)
+        {
+            switch (academicPosition)
+            {
+                case AcademicPosition.AssistantProfessor:
+                    return "Asst. Prof.";
+                case AcademicPosition.SeniorResearcher:
+                    return "Sr. Rsch.";
+                case AcademicPosition.AssociateProfessor:
+                    return "Assoc. Prof.";
+                case AcademicPosition.Professor:
+                    return "Prof.";
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
