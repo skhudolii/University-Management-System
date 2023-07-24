43f8b52f2da459b806cf0bde7ebc53ad6895a68c
Serhii Khudolii <s.khudolii@gmail.com>
2023-07-24 13:47:25 +0300

    Add validation for NewLectureVM.LectureDate, NewLectureVM.StartTime, NewLectureVM.EndTime using custom Attributes

--- src/University.Core/ViewModels/LectureVM/NewLectureVM.cs
+++ src/University.Core/ViewModels/LectureVM/NewLectureVM.cs
@@ -1,42 +1,46 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using University.Core.ViewModels.Base;
+using University.Core.Attributes;
 
 namespace University.Core.ViewModels.LectureVM
 {
     public class NewLectureVM : BaseViewModel
     {
-        [Display(Name = "Lecture date")]
-        [Required(ErrorMessage = "Date is required")]
+        [Display(Name = "Lecture date")]        
         [DataType(DataType.Date)]
         [Column(TypeName = "Date")]
         [DisplayFormat(DataFormatString = "{0:ddd, dd MMM yy}")]
+        [Required(ErrorMessage = "{0} is required")]
+        [LectureDate(ErrorMessage = "{0} cannot be before today")]
         public DateTime LectureDate { get; set; }
 
         [Display(Name = "Start time")]
-        [Required(ErrorMessage = "Start time is required")]
         [DataType(DataType.Time)]
+        [Required(ErrorMessage = "{0} is required")]
+        [StartTime(ErrorMessage = "{0} cannot be before the current time for today's lecture")]        
         public TimeSpan StartTime { get; set; }
 
-        [Display(Name = "End time")]
-        [Required(ErrorMessage = "End time is required")]
+        [Display(Name = "End time")]        
         [DataType(DataType.Time)]
+        [Required(ErrorMessage = "{0} is required")]
+        [EndTime(StartTimePropertyName = "StartTime", ErrorMessage = "{0} must be greater than start time")]
         public TimeSpan EndTime { get; set; }
 
         [Display(Name = "Faculty")]
-        [Required(ErrorMessage = "Faculty is required")]
+        [Required(ErrorMessage = "{0} is required")]
         public int? FacultyId { get; set; }
 
         [Display(Name = "Subject")]
-        [Required(ErrorMessage = "Subject is required")]
+        [Required(ErrorMessage = "{0} is required")]
         public int? SubjectId { get; set; }
 
         [Display(Name = "Lecture room")]
-        [Required(ErrorMessage = "Lecture room is required")]
+        [Required(ErrorMessage = "{0} is required")]
         public int? LectureRoomId { get; set; }
 
         [Display(Name = "Teacher")]
-        [Required(ErrorMessage = "Teacher is required")]
+        [Required(ErrorMessage = "{0} is required")]
         public int? AcademicEmployeeId { get; set; }
  
         [Display(Name = "Group(s) - Ctrl and click to multiple choose")]
